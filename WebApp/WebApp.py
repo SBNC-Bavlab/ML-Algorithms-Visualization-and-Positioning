cacce59a93409527e653a6eb8c45884e6a440ee5
Ziya Erkoc <ziya.erkoc@ug.bilkent.edu.tr>
2018-06-27 13:31:39 +0300

    minor change
    -html changed
    -graph added instead of histogram

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request
 import pandas as pd
-from bokeh.charts import Histogram
 from bokeh.plotting import figure
 from bokeh.embed import components
+
+import math
+
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+
+import networkx as nx
+
+from bokeh.io import show, output_file
+from bokeh.models import Plot, Range1d, MultiLine, Circle, HoverTool, TapTool, BoxSelectTool
+from bokeh.models.graphs import from_networkx, NodesAndLinkedEdges, EdgesAndLinkedNodes
+from bokeh.palettes import Spectral4
 
 app = Flask(__name__)
 
@@ -16,15 +29,53 @@ feature_names = iris_df.columns[0:-1].values.tolist()
 def create_figure(current_feature_name, bins):
     #p = figure(plot_width=600, plot_height=400)
     #p.vbar(x = "asd")
-     p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species',
-                   bins=bins, legend='top_right', width=600, height=400)
+#     p = Histogram(iris_df, current_feature_name, title=current_feature_name, color='Species',
+#                   bins=bins, legend='top_right', width=600, height=400)
+#    
+#     # Set the x axis label
+#     p.xaxis.axis_label = current_feature_name
+#    
+#     # Set the y axis label
+#     p.yaxis.axis_label = 'Count'
+    N = 8
+    node_indices = list(range(N))
     
-     # Set the x axis label
-     p.xaxis.axis_label = current_feature_name
+    plot = figure(title="Graph Layout Demonstration", x_range=(-1.1,1.1), y_range=(-1.1,1.1),
+                  tools="", toolbar_location=None)
     
-     # Set the y axis label
-     p.yaxis.axis_label = 'Count'
-     return p
+    graph = GraphRenderer()
+    
+    graph.node_renderer.data_source.add(node_indices, 'index')
+    graph.node_renderer.data_source.add(Spectral8, 'color')
+    graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color="color")
+    
+    graph.edge_renderer.data_source.data = dict(
+        start=[0]*N,
+        end=node_indices)
+    
+    ### start of layout code
+    circ = [i*2*math.pi/8 for i in node_indices]
+    x = [math.cos(i) for i in circ]
+    y = [math.sin(i) for i in circ]
+    graph_layout = dict(zip(node_indices, zip(x, y)))
+    graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+    
+    ### Draw quadratic bezier paths
+    def bezier(start, end, control, steps):
+        return [(1-s)**2*start + 2*(1-s)*s*control + s**2*end for s in steps]
+    
+    xs, ys = [], []
+    sx, sy = graph_layout[0]
+    steps = [i/100. for i in range(100)]
+    for node_index in node_indices:
+        ex, ey = graph_layout[node_index]
+        xs.append(bezier(sx, ex, 0, steps))
+        ys.append(bezier(sy, ey, 0, steps))
+    graph.edge_renderer.data_source.data['xs'] = xs
+    graph.edge_renderer.data_source.data['ys'] = ys
+    
+    plot.renderers.append(graph)
+    return plot
 
 
 # Index page
