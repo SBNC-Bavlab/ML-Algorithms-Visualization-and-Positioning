29aaf749b02a945e6cfada6d6830f787e23b53be
Ziya Erkoc <ziya.erkoc@ug.bilkent.edu.tr>
2018-07-11 10:34:56 +0300

    -'4' key error fixed

--- Bokeh/Plot/dictionaries.py
+++ Bokeh/Plot/dictionaries.py
@@ -36,18 +36,18 @@ label_to_tr = {
         "buyingAttr": {"vhigh": "Çok Yüksek", "high": "Yüksek", "med": "Orta", "low": "Düşük"},
         "maintAttr": {"vhigh": "Çok Yüksek", "high": "Yüksek", "med": "Orta", "low": "Düşük"},
         "doorsAttr": {"2": "2", "3": "3", "4": "4", "5more": "5 ve fazlası"},
-        "personsAttr": {"2": "2", "3": "3", "more": "daha fazla"},
+        "personsAttr": {"2": "2", "4": "4", "more": "daha fazla"},
         "lug_bootAttr": {"small": "küçük", "med": "orta", "big": "büyük"},
         "safetyAttr": {"low": "düşük", "med": "orta", "high": "yüksek"},
         "classAttr": {"-": "Yok", "unacc": "Zor Satılır", "acc": "Belki Satılır", "good": "Satılır", "vgood": "Kolayca Satılır"}
     },
     "GoT": {
-            "Zengin mi?": {"Zengin" : "Zengin", "Zengin değil" : "Zengin değil"},
-            "Ejderhası var mı?": {"Var" : "Var", "Yok" : "Yok"},
-            "Tahtı istiyor mu?": {"İstiyor" : "İstiyor", "İstemiyor" : "İstemiyor"},
-            "Saç rengi": {"Sarı" : "Sarı", "Siyah" : "Siyah", "Beyaz" : "Beyaz", "Kızıl" : "Kızıl"},
-            "Ailesi": {"Lannister" : "Lannister", "Stark" : "Stark", "White Walker" : "White Walker", "Targaryen" : "Targaryen"}
-        }
+        "Zengin mi?": {"Zengin" : "Zengin", "Zengin değil" : "Zengin değil"},
+        "Ejderhası var mı?": {"Var" : "Var", "Yok" : "Yok"},
+        "Tahtı istiyor mu?": {"İstiyor" : "İstiyor", "İstemiyor" : "İstemiyor"},
+        "Saç rengi": {"Sarı" : "Sarı", "Siyah" : "Siyah", "Beyaz" : "Beyaz", "Kızıl" : "Kızıl"},
+        "Ailesi": {"Lannister" : "Lannister", "Stark" : "Stark", "White Walker" : "White Walker", "Targaryen" : "Targaryen"}
+    }
 }
 attr_to_turkish = {
     "lens": {
