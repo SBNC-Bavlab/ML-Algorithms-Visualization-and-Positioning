8137451c7e28d61d9189acb39714a9011e5709e5
Ziya Erkoc <ziya.erkoc@ug.bilkent.edu.tr>
2018-07-12 19:46:09 +0300

    -plot decision tree beautified

--- Bokeh/K-Means/kmeans_cluestering.py
+++ Bokeh/K-Means/kmeans_cluestering.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from sklearn.preprocessing import normalize
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,17 +10,23 @@ import pandas as pd
 from sklearn.datasets.samples_generator import make_blobs
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import row
-X, y = make_blobs()
+#X, y = make_blobs()
+df = pd.read_csv("../Data/customer.csv")
+
+xs = df['Milk'].values
+ys = df['Delicassen'].values
+X = np.array(list(zip(xs, ys)))
+X = normalize(X)
 def create_figure(random_cluster = True):
     TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
     p = figure(tools=TOOLS)
 
     if random_cluster:
         p.title.text = "A"
-        kmeans = KMeans(n_clusters=3)
+        kmeans = KMeans(n_clusters=2)
     else:
         p.title.text = "B"
-        kmeans = KMeans(n_clusters=3, init=np.array([[5,5],[5,5],[5,5]]))
+        kmeans = KMeans(n_clusters=4)#init=np.array([[5,5],[5,5],[5,5]]))
     kmeans.fit(X)
 
     cluster_xs = kmeans.cluster_centers_[:, 0]
@@ -35,12 +42,12 @@ def create_figure(random_cluster = True):
 
     dataSource = ColumnDataSource(data=pd.DataFrame.from_dict(df))
 
-    p.circle(x='x', y='y', size = 5, source=dataSource,  color=factor_cmap('cluster', palette=list(cmap.values()), factors=list(cmap.keys())))
+    p.circle(x='x', y='y', size=5, source=dataSource,  color=factor_cmap('cluster', palette=list(cmap.values()), factors=list(cmap.keys())))
 
-    p.rect(cluster_xs, cluster_ys, 0.2, 0.2, color="black")
+    p.rect(cluster_xs, cluster_ys, 5, 5, width_units = "screen", height_units = "screen", color="black")
 
 
     return p;
 p = create_figure()
-p2 = create_figure(False)
-show(row(p, p2))
+#p2 = create_figure(False)
+show(row(p))
