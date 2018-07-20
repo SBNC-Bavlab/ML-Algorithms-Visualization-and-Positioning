916beb814ca547412087a8c139dc2055f025477d
Ziya Erkoc <ziya.erkoc@ug.bilkent.edu.tr>
2018-07-20 10:31:10 +0300

    -vote visualized -csv created

--- Bokeh/K-Means/kmeans_cluestering.py
+++ Bokeh/K-Means/kmeans_cluestering.py
@@ -11,28 +11,86 @@ from sklearn.datasets.samples_generator import make_blobs
 from bokeh.models import ColumnDataSource
 from bokeh.layouts import row
 
-with open("../Data/ilceler.txt") as file:
-    for line in file:
-        print(line)
 """
+districts_df = pd.read_csv("C:/Users/ASUS/Desktop/YSKSecimSonuclari-master/YSKScraper/Data/geocodedAddresses2015.csv")
 votes = pd.DataFrame.from_csv("C:/Users/ASUS/Desktop/YSKSecimSonuclari-master/YSKScraper/Data/yskCombined2015.csv")
 votes.columns = [col.strip() for col in votes.columns]
-votes.drop(["SECMEN_SAYISI", "BIRIM_ID", "MUHTARLIK_ID"], axis=1, inplace=True)
-print(votes.columns)
-print(votes.where(votes["İL ADI"] == " ADANA").head());
-X, y = make_blobs()
 
-def create_figure(random_cluster = True):
-    TOOLS = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
-    p = figure(tools=TOOLS)
+# Drop unnecessary columns
+votes.drop(["ITIRAZSIZ_GECERLI_OY_SAYISI", "ITIRAZLI_GECERLI_OY_SAYISI", "BIRIM_ID", "MUHTARLIK_ID", "Mahalle / Köy", "SANDIK NO"]
+           + [deleted_cols for i, deleted_cols in enumerate(votes.columns) if i > 11], axis=1, inplace=True)
 
-    if random_cluster:
-        p.title.text = "A"
-        kmeans = KMeans(n_clusters=2)
-    else:
-        p.title.text = "B"
-        kmeans = KMeans(n_clusters=4)#init=np.array([[5,5],[5,5],[5,5]]))
+# None values are found and dropped
+districts_df = districts_df.replace(' ---', np.nan).dropna()
+#Enlem and boylam are casted to float as they will be mean()'ed
+districts_df["ENLEM"] = districts_df["ENLEM"].astype(float)
+districts_df["BOYLAM"] = districts_df["BOYLAM"].astype(float)
+# Unnecesarry col dropped
+districts_df.drop(["MAHALLE"], axis=1)
+
+# Unecesarry rows that are mistakenly put by the creator of the dataset, dropped
+votes = votes.drop(votes[votes.OY_KULLANAN_SECMEN_SAYISI == " OY_KULLANAN_SECMEN_SAYISI"].index)
+# Some attributes are casted to int as sum() function will be applied to them
+votes["OY_KULLANAN_SECMEN_SAYISI"] = votes["OY_KULLANAN_SECMEN_SAYISI"].astype(int)
+votes["SECMEN_SAYISI"] = votes["SECMEN_SAYISI"].astype(int)
+votes["GECERLI_OY_TOPLAMI"] = votes["GECERLI_OY_TOPLAMI"].astype(int)
+votes["GECERSIZ_OY_TOPLAMI"] = votes["GECERSIZ_OY_TOPLAMI"].astype(int)
+
+grouped1 = votes.groupby(["İL ADI", "İLÇE ADI"], as_index=False).sum()
+grouped2 = districts_df.groupby(["İL ADI", "İLÇE ADI"], as_index=False).mean()
+final_df = pd.merge(grouped1, grouped2[["İLÇE ADI", "ENLEM", "BOYLAM"]], on=["İLÇE ADI"])
+
+# Final adjustments
+final_df["İLÇE ADI"] = [row.strip() for row in final_df["İLÇE ADI"]]
+final_df["İL ADI"] = [row.strip() for row in final_df["İL ADI"]]
+final_df["GECERSIZ_OY_ORAN"] = [invalid / (invalid + valid) * 100
+                                for valid, invalid
+                                in zip(final_df["GECERLI_OY_TOPLAMI"], final_df["GECERSIZ_OY_TOPLAMI"])]
+final_df.to_csv("bilmem_ne.csv", sep=",", header=True, index=False, encoding="utf-8")
+"""
+"""
+for val in grouped['İLÇE ADI'].index:
+    district_name = grouped.iloc[val]['İLÇE ADI']
+    city = grouped.iloc[val]['İL ADI']
+    lat = "-"
+    long = "-"
+    val2 = 0
+    while val2 < len(districts_df['İLÇE ADI']):
+        district_name_2 = districts_df.iloc[val2]['İLÇE ADI']
+        if district_name_2.strip() == district_name.strip():
+            lat = districts_df.iloc[val2]["ENLEM"]
+            long = districts_df.iloc[val2]["BOYLAM"]
+            districts_df = districts_df.drop(districts_df.index[val2])
+            valids += 1
+            break
+        val2 += 1;
+    grouped.ix[val, 'ENLEM'] = lat
+    grouped.ix[val, 'BOYLAM'] = long
+print(len(grouped) - valids)
+grouped.to_csv("bilmem_ne.csv", sep=",", index=False, header=False, encoding="utf-8")
+"""
+# print(grouped)
+
+
+secim_df = pd.read_csv("../Data/bilmem_ne.csv")
+print(secim_df)
+X = [[x, y] for x, y in zip(secim_df["BOYLAM"], secim_df["GECERSIZ_OY_ORAN"])]
+X2 = [[x, y] for x, y in zip(secim_df["GECERSIZ_OY_ORAN"], secim_df["ENLEM"])]
+X3 = [[x, y] for x, y in zip(secim_df["BOYLAM"], secim_df["ENLEM"])]##secim_df["BOYLAM"], secim_df["ENLEM"])]
+
+
+def create_figure(X):
+    tools = "hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo," \
+            "redo,reset,tap,save,box_select,poly_select,lasso_select,"
+    p = figure(tools=tools, tooltips=[("Enlem", "@x"), ("İlçe Adı", "@ilce_adi"),
+                                      ("Geçersiz Oy Oranı", "@gecersiz_oran"), ("Boylam", "@y")])
+
+    p.title.text = "Seçim"
+    kmeans = KMeans(n_clusters=4)
     kmeans.fit(X)
+
+    districts = secim_df["İLÇE ADI"].tolist()
+    invalid_ratio = secim_df["GECERSIZ_OY_ORAN"].tolist()
 
     cluster_xs = kmeans.cluster_centers_[:, 0]
     cluster_ys = kmeans.cluster_centers_[:, 1]
@@ -40,8 +98,9 @@ def create_figure(random_cluster = True):
     columns = ['x','y','cluster']
     df = pd.DataFrame(columns=columns)
 
-    for  index, coords in enumerate(X):
-        df = df.append({'x': coords[0], 'y': coords[1], 'cluster': str(kmeans.labels_[index])}, ignore_index=True)
+    for index, coords in enumerate(X):
+        df = df.append({'x': coords[0], 'y': coords[1], 'cluster': str(kmeans.labels_[index]),
+                       "ilce_adi": districts[index], "gecersiz_oran": invalid_ratio[index]}, ignore_index=True)
 
     cmap = {'0': "red", '1': "green", '2': "blue", '3': "orange", '4': "beige"}
 
@@ -49,10 +108,10 @@ def create_figure(random_cluster = True):
 
     p.circle(x='x', y='y', size=5, source=dataSource,  color=factor_cmap('cluster', palette=list(cmap.values()), factors=list(cmap.keys())))
 
-    p.rect(cluster_xs, cluster_ys, 5, 5, width_units = "screen", height_units = "screen", color="black")
+    p.rect(cluster_xs, cluster_ys, 10, 10, width_units = "screen", height_units = "screen", color="black")
+    return p
 
-
-    return p;
-p = create_figure()
-#p2 = create_figure(False)
-show(row(p))"""
+p = create_figure(X)
+p2 = create_figure(X2)
+p3 = create_figure(X3)
+show(row(p, p2, p3))
