c35f83f3701998c0410eb1afcbc08136b3ada966
liber <e2035632@ceng.metu.edu.tr>
2018-08-02 16:44:07 +0300

    mnist ann

--- Bokeh/ANN/Plot/ann_data_instance.py
+++ Bokeh/ANN/Plot/ann_data_instance.py
@@ -7,11 +7,9 @@ class ANNData():
     def __init__(self, learning_rate, activation_func, layers):
         self.learning_rate = learning_rate
         self.activation_func = activation_func
-        self.attr_list = []
         self.layers = layers
 
-    def update(self, learning_rate, activation_func, attr_list, layers):
+    def update(self, learning_rate, activation_func, layers):
         self.learning_rate = learning_rate
         self.activation_func = activation_func
-        self.attr_list = attr_list
         self.layers = layers
