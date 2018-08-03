438afd86b43012065388170d6860d8486ce34e74
evrimozel <evrozel@gmail.com>
2018-08-03 10:30:44 +0300

    visual

--- Bokeh/ANN/Plot/ann_data_instance.py
+++ Bokeh/ANN/Plot/ann_data_instance.py
@@ -4,14 +4,14 @@ Class for storing ANN data
 
 
 class ANNData():
-    def __init__(self, learning_rate, activation_func, layers):
+    def __init__(self, learning_rate, activation_func, layers, epoch):
         self.learning_rate = learning_rate
         self.activation_func = activation_func
-        self.attr_list = []
         self.layers = layers
+        self.epoch = epoch
 
-    def update(self, learning_rate, activation_func, attr_list, layers):
+    def update(self, learning_rate, activation_func, layers, epoch):
         self.learning_rate = learning_rate
         self.activation_func = activation_func
-        self.attr_list = attr_list
         self.layers = layers
+        self.epoch = epoch
--- Bokeh/ANN/main.py
+++ Bokeh/ANN/main.py
@@ -1,94 +1,205 @@
 from bokeh.plotting import figure
-from bokeh.models.widgets import Select, CheckboxGroup, Paragraph, Button
+from bokeh.models.widgets import Select, Button, Slider, Paragraph
 from bokeh.layouts import layout, widgetbox
 from Plot.ann_data_instance import ANNData
 from bokeh.io import curdoc
-
-all_attrs_list = ["1", "2", "3", "4"]
+from bokeh.models import ColumnDataSource
 
 learning_rate_select = Select(title="Öğrenme Hızını Seçiniz:", options=['1e-5', '1e-3', '0.1', '1'], value="1e-5")
 activation_func_select = Select(title="Aktivasyon Fonksiyonunu Seçiniz:", options=['ReLu', 'Tanh', 'Sigmoid'],
                                 value='ReLu')
-attr_info = Paragraph(text="Nitelikleri seçiniz:")
-attribute_checkbox = CheckboxGroup(labels= all_attrs_list,
-                                   active=[0, 1, 2, 3])
 add_layer_button = Button(label="Katman Ekle", button_type="success")
-remove_layer_button = Button(label="Katman Çıkar", button_type="success")
-add_node_button = Select(title="Seçtiğiniz Katmana Node Ekle", options=["1", "2", "3", "4"])
-remove_node_button = Select(title="Seçtiğiniz Katmandan Node Çıkar", options=["1", "2", "3", "4"])
-widgets = widgetbox(learning_rate_select, activation_func_select, attr_info, attribute_checkbox,
-                    add_layer_button, remove_layer_button,
-                    add_node_button, remove_node_button, sizing_mode="stretch_both")
+remove_layer_select = Select(title="Katman Çıkar:", options=["Seçiniz", "1"])
+layer_select = Select(title="Düğüm Eklemek/Çıkarmak İçin Katman Seç:", options=["1"])
+add_node_button = Button(label="+", button_type="warning")
+remove_node_button = Button(label="-", button_type="warning")
+epoch_slider = Slider(start=500, end=50000, value=500, step=1000, title="Adım Sayısı Belirle")
+play_button = Button(label="Oynat", button_type="success")
+wait_info = Paragraph(text="")
+widgets = widgetbox(learning_rate_select, activation_func_select,
+                    add_layer_button, remove_layer_select, layer_select,
+                    add_node_button, remove_node_button, epoch_slider, play_button, wait_info, sizing_mode="stretch_both")
 
-ANN = ANNData(learning_rate_select.value, activation_func_select.value, layers=[[3]])
+ANN = ANNData(learning_rate_select.value, activation_func_select.value, layers=[3, 10], epoch=500)
+layer_num = 2
+circle_radius = 10
+
+current_layer = "1"
+
+
+circle_source = ColumnDataSource(data={'x': [], 'y': []})
+arrow_source = ColumnDataSource(data={"x_start": [], "x_end": [], "y_start": [], "y_end": [], "xs": [], "ys": []})
 
 p = figure(toolbar_location=None)
+p.outline_line_color = "white"
+p.axis.visible = False
 p.grid.grid_line_color = None
-p.legend.orientation = "vertical"
-p.legend.location = "top_right"
 
-visual = layout([[widgets, p]])
+circles = p.circle("x", "y", radius=circle_radius, radius_units='screen', source=circle_source, name="circles",
+         color="lightseagreen")
+p.multi_line(line_alpha=0.7, line_color="darkgray", name="multi_lines", xs="xs", ys="ys", source=arrow_source,
+             color="lightseagreen")
+#p.rect(0.5, 5, 0.1, 10, color="lightgray")
+#p.add_layout(Arrow(end=NormalHead(fill_color="lightgray", line_color="lightgray"), line_color="lightgray", x_start=0.5, y_start=5, x_end=0.75, y_end=5, line_width=10))
+visual = layout([
+    [widgets, p],
+])
+
+
+def apply_changes(layers):
+    circle_data = {'x': [], 'y': []}
+    arrow_data = {"x_start": [], "x_end": [], "y_start": [], "y_end": [], "xs": [], "ys": []}
+
+    x_offset = 1
+    for i in range(len(layers)):
+        y_offset = (10 / layers[i]) / 2
+        for j in range(layers[i]):
+            circle_data['x'].append(x_offset)
+            circle_data['y'].append(y_offset)
+            y_offset += (10/layers[i])
+        x_offset += 1
+    circle_source.data = circle_data
+
+    if len(layers) > 1:
+        x_offset = 1
+        for i in range(len(layers)):
+            y_offset = (10 / layers[i]) / 2
+            for j in range(layers[i]):
+                if i+1 != len(layers):
+                    y_next_offset = (10 / layers[i+1]) / 2
+                    for index in range(layers[i+1]):
+                        arrow_data['x_start'].append(x_offset)
+                        arrow_data['x_end'].append(x_offset + 1)
+                        arrow_data['y_start'].append(y_offset)
+                        arrow_data['y_end'].append(y_next_offset)
+                        y_next_offset+= (10/layers[i+1])
+                y_offset += (10 / layers[i])
+            x_offset += 1
+
+    arrow_data["xs"] = [[x_start] for x_start in arrow_data["x_start"]]
+    for i in range(len(arrow_data["x_end"])):
+        arrow_data["xs"][i] += [arrow_data["x_end"][i]]
+    arrow_data["ys"] = [[y_start] for y_start in arrow_data["y_start"]]
+    for i in range(len(arrow_data["y_end"])):
+        arrow_data["ys"][i] += [arrow_data["y_end"][i]]
+
+    arrow_source.data = arrow_data
+
+
+apply_changes(ANN.layers)
 
 
 def change_learning_rate(_attr, _old, new):
-    ANN.update(new, ANN.activation_list, ANN.attr_list, ANN.layers)
+    ANN.update(new, ANN.activation_func, ANN.layers, ANN.epoch)
+    apply_changes(ANN.layers)
 
 
 learning_rate_select.on_change("value", change_learning_rate)
 
 
 def change_activation_func(_attr, _old, new):
-    ANN.update(ANN.learning_rate, new, ANN.attr_list, ANN.layers)
+    ANN.update(ANN.learning_rate, new, ANN.layers, ANN.epoch)
 
 
 activation_func_select.on_change("value", change_activation_func)
 
 
-def change_attributes(new):
-    active_attributes_list = []
-    for i in new:
-        active_attributes_list.append(all_attrs_list[i])
-    ANN.update(ANN.learning_rate, ANN.activation_func, active_attributes_list, ANN.layers)
-
-
-attribute_checkbox.on_click(change_attributes)
-
-
 def add_layer():
-    ANN.update(ANN.learning_rate, ANN.activation_func, ANN.attr_list, ANN.layers+[[3]])
-    print(ANN.layers)
+    global layer_num, current_layer
+    if len(ANN.layers) == 10:
+        return
+    layer = ANN.layers[:-1]
+    layer += [3]
+    layer += [ANN.layers[-1]]
+    ANN.update(ANN.learning_rate, ANN.activation_func, layer, ANN.epoch)
+
+    if not layer_select.options:
+        add_node_button.disabled = False
+        remove_node_button.disabled = False
+        current_layer = "1"
+
+    layer_select.options = layer_select.options + [str(layer_num)]
+    remove_layer_select.options = remove_layer_select.options + [str(layer_num)]
+    layer_num += 1
+    apply_changes(ANN.layers)
 
 
 add_layer_button.on_click(add_layer)
 
 
-def remove_layer():
-    ANN.update(ANN.learning_rate, ANN.activation_func, ANN.attr_list, ANN.layers[:-1])
-    print(ANN.layers)
-
-
-remove_layer_button.on_click(remove_layer)
-
-
-def add_node(_attr, _old, new):
+def remove_layer(_attr, _old, new):
+    global layer_num
+    if new == "Seçiniz":
+        return
+    elif len(remove_layer_select.options) == 2:
+        remove_layer_select.value = "Seçiniz"
+        return
     layer = ANN.layers
-    new = int(new)
+    layer.pop(int(new)-1)
+    ANN.update(ANN.learning_rate, ANN.activation_func, layer, ANN.epoch)
+    remove_layer_select.value = "Seçiniz"
+    layer_num -= 1
+
+    options = ["Seçiniz"]
+    for i in range(layer_num-1):
+        options.append(str(i+1))
+
+    layer_select.options = options[1:]
+    if not layer_select.options:
+        add_node_button.disabled = True
+        remove_node_button.disabled = True
+    remove_layer_select.options = options
+    apply_changes(ANN.layers)
+
+
+remove_layer_select.on_change("value", remove_layer)
+
+
+def choose_layer(_attr, _old, new):
+    global current_layer
+    current_layer = new
+
+
+layer_select.on_change("value", choose_layer)
+
+
+def add_node():
+    if ANN.layers[int(current_layer)-1] == 10:
+        return
+    layer = ANN.layers
+    layer[int(current_layer)-1] += 1
+    ANN.update(ANN.learning_rate, ANN.activation_func, layer, ANN.epoch)
+    apply_changes(ANN.layers)
+
+
+add_node_button.on_click(add_node)
+
+
+def remove_node():
+    if ANN.layers[int(current_layer)-1] == 1:
+        return
+    layer = ANN.layers
+    layer[int(current_layer) - 1] -= 1
+    ANN.update(ANN.learning_rate, ANN.activation_func, layer, ANN.epoch)
+    apply_changes(ANN.layers)
+
+
+remove_node_button.on_click(remove_node)
+
+
+def choose_epoch(_attr, _old, new):
+    ANN.update(ANN.learning_rate, ANN.activation_func, ANN.layers, int(new))
     print(new)
-    layer[new-1] += 1
-    ANN.update(ANN.learning_rate, ANN.activation_func, ANN.attr_list, layer)
-    print(ANN.layers)
 
 
-add_node_button.on_change("value", add_node)
+epoch_slider.on_change("value", choose_epoch)
 
 
-def remove_node(_attr, _old, new):
-    layer = ANN.layers
-    layer[int(new)-1] -= 1
-    ANN.update(ANN.learning_rate, ANN.activation_func, ANN.attr_list, layer)
-    print(ANN.layers)
+def play():
+    wait_info.text = "Bekleyiniz..."
 
 
-remove_node_button.on_change("value", remove_node)
+play_button.on_click(play)
+
 
 curdoc().add_root(visual)
